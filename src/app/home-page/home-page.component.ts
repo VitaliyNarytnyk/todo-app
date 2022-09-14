a58cb4f97d4938b66db010c3905def3f3fc21da7
VitaliyNarytnyk <vitaliy.narytnyk@gmail.com>
2022-09-15 00:53:18 +0300

    created streams for filtering

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable, Subscription } from 'rxjs';
 import { Todo } from '../shared/interfaces';
+import { FilterEnum } from '../shared/services/filter.enum';
 import { TodosService } from '../shared/services/todos.servics';
 
 @Component({
@@ -12,12 +13,24 @@ export class HomePageComponent implements OnInit {
 
   todos$!: Observable<Todo[]>
 
+  visibleTodos$!: Observable<Todo[]>
+
   constructor(
     private todosService: TodosService
   ) { }
 
   ngOnInit(): void {
-    this.todos$ = this.todosService.getAll()
+    this.visibleTodos$ = combineLatest(
+      this.todos$ = this.todosService.getAll(),
+      this.todosService.filter$
+    ).pipe(map(([todos, filter]: [Todo[], FilterEnum]) => {
+      if (filter === FilterEnum.active) {
+        return todos.filter((todo) => !todo.status)
+      } else if (filter === FilterEnum.completed) {
+        return todos.filter((todo) => todo.status)
+      }
+      return todos
+    }))
   }
 
 }
